7fb22f5f4928e14c06596a020a308e0d78347b6d
Langli <Langliland64@gmail.com>
2024-06-09 16:45:26 -0700

    font and colors initialized

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        halfxl: "1440px"
+      },
+      colors: {
+        primary: 'hsl(172, 67%, 45%)',
+        secondary_100: 'hsl(0, 0%, 100%)',
+        secondary_200: 'hsl(189, 41%, 97%)',
+        secondary_300: 'hsl(185, 41%, 84%)',
+        secondary_500: 'hsl(184, 14%, 56%)',
+        secondary_700: 'hsl(186, 14%, 43%)',
+        secondary_800: 'hsl(183, 100%, 15%)'
+      }
+    },
   },
   plugins: [],
 }
